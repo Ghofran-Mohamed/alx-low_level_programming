79c0dbdad80fb5db61d48be25612283d1a46fe09
Ghofan-Mohamed <ghfranzaher2003@gmail.com>
2023-07-13 13:08:27 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-const char *message = "and that piece of art is useful\" -, "
+const char *message = "and that piece of art is useful\" -  "
 		"Dora Korpar, 2015-10-19\n";
 
 ssize_t bytes_written = write(STDERR_FILENO, message, 59);

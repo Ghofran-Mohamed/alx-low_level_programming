88e3d9606b238351f92a8c6341ff5812d6a13978
Ghofan-Mohamed <ghfranzaher2003@gmail.com>
2023-07-13 12:36:45 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-#include <string.h>
 /**
  * main - Entry point
  *
@@ -7,11 +6,12 @@
  */
 int main(void)
 {
-const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-size_t length = strlen(message);
-ssize_t bytes_written = write(STDERR_FILENO, message, length);
-if (bytes_written == -1) {
-return 1;
-    }
-return 1;
+const char *message = "and that piece of art is useful\" - "
+	"Dora Korpar, 2015-10-19\n";
+ssize_t bytes_written = write(STDERR_FILENO, message, 59);
+if (bytes_written == -1)
+{
+return (1);
+}
+return (1);
 }

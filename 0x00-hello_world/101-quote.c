28667401b96ee600ce3facc82018376740cb0f31
Ghofan-Mohamed <ghfranzaher2003@gmail.com>
2023-07-13 11:11:15 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+size_t length = strlen(message);
+ssize_t bytes_written = write(STDERR_FILENO, message, length);
+if (bytes_written == -1) {
+return 1;
+    }
+return 1;
+}
